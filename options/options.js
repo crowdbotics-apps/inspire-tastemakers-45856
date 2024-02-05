03387fc8e7b546132fb650ab2c4b3489c04e25be
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:19:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "inspire-tastemakers-45856",
+  url: "https://inspire-tastemakers-45856.botics.co",
+  api: "https://inspire-tastemakers-45856.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
